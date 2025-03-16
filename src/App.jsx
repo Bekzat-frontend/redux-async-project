20a15e890a77e6db175bc94b3de189ccc24394b9
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-16 21:08:23 +0700

    spinner

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function App() {
-  const { courses, error, isPending } = useSelector((state) => {
-    console.log(state);
-  });
-  return <div></div>;
+  const { courses, error, isPending } = useSelector((state) => state.courses);
+  return (
+    <div>
+      {!isPending ? <span className="loader"></span> : ""}
+      {!error ? <p>кечирес маалымат жок</p> : ""}
+    </div>
+  );
 }
 
 export default App;
